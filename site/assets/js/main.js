6fde6c7fc0e1aa415927b37b9c0402c5cfee8257
Tiago Farias <farias.tiago.ti@gmail.com>
2026-06-20 08:42:50 -0300

    Fix SEO Open Graph tags and WhatsApp placeholder number.

--- site/assets/js/main.js
+++ site/assets/js/main.js
@@ -1,7 +1,7 @@
 (function () {
   'use strict';
 
-  var WHATSAPP_NUMBER = '5551991213724';
+  var WHATSAPP_NUMBER = '5554999999999';
   var CITY_NAME = 'Salto do Yucumã';
   var prefersReducedMotion = window.matchMedia('(prefers-reduced-motion: reduce)').matches;
 
